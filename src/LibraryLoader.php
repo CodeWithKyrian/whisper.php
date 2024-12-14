ccc650db079e1301aeeb6ecc8173a8306a631c11
Kyrian Obikwelu <koshnawaza@gmail.com>
2024-12-14 17:11:08 +0100

    feat: Use production package installer

--- src/LibraryLoader.php
+++ src/LibraryLoader.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Codewithkyrian\Whisper;
 
-use Codewithkyrian\ComposerPlatformPackages\Platform;
+use Codewithkyrian\PlatformPackageInstaller\Platform;
 use FFI;
 use RuntimeException;
 
